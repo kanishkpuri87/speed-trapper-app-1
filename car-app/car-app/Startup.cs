2e1a73b4608f1fc2d44cddeda25f87d93d75d3e1
kanishkpuri87 <kanishkpuri@ymail.com>
2014-10-20 13:57:22 +0100

    car-app

---
+++ car-app/car-app/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(car_app.Startup))]
+namespace car_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HubConfiguration();
+            
+            config.EnableJSONP = true;
+            config.EnableDetailedErrors = true;
+
+
+            app.MapSignalR(config);
+        }
+    }
+}
---
+++ speed-trapper-app/speed-trapper/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(speed_trapper.Startup))]
+namespace speed_trapper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            var config = new HubConfiguration();
+            config.EnableJSONP = true;
+            config.EnableDetailedErrors = true;
+            
+            
+            app.MapSignalR(config);
+        }
+    }
+}
